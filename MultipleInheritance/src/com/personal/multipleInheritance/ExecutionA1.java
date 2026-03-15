03b086f584bfefc540d04a3a4969b1d9f4a88fd8
Anshuman Sharma <162892791+anshumansharmaa@users.noreply.github.com>
2026-03-15 20:15:13 +0530

    Update ExecutionA1.java

--- MultipleInheritance/src/com/personal/multipleInheritance/ExecutionA1.java
+++ MultipleInheritance/src/com/personal/multipleInheritance/ExecutionA1.java
@@ -1,9 +1,13 @@
-package com.personal.multipleInheritance;
+package com.personal.multipleinheritance;
 
-public class ExecutionA1 {
-	public static void main(String[] args) {
-		Information I = new Information();
-		I.speak();
-		I.sound();
-	}
+public class Application {
+
+    public static void main(String[] args) {
+
+        Human human = new LivingBeing();
+        human.speak();
+
+        Animal animal = new LivingBeing();
+        animal.makeSound();
+    }
 }
